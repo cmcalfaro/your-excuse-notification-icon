caab3f7868bef070f89d6a81b3ad951d927a1849
cmcalfaro <carlos.alfaro@gmail.com>
2024-10-16 19:37:44 +0100

    Create your-excuse-notification-icon.java

---
+++ your-excuse-notification-icon/your-excuse-notification-icon.java
@@ -0,0 +1,44 @@
+package com.qrglue.excuse.android;
+
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.Hashtable;
+import java.util.List;
+
+import org.apache.cordova.CordovaArgs;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaInterface;
+import org.apache.cordova.CordovaWebView;
+import org.apache.cordova.PluginResult;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
+import android.os.RemoteException;
+import android.util.Log;
+
+
+public class your_excuse_notification_icon extends CordovaPlugin {
+
+	@Override
+	public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+		super.initialize(cordova, webView);		
+	}
+
+	@Override
+	public boolean execute(String action, CordovaArgs args, CallbackContext callbackContext) throws JSONException {
+		return true;
+	}
+
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+	}
+
+}
